e6430ddc6d22ee70ef74a142396ea9e98252a277
Soulrender268451 <as20240565@sci.sjp.ac.lk>
2025-10-26 20:34:22 +0530

    Adding Manage Cities function

--- main.c
+++ main.c
@@ -25,3 +25,75 @@ void showCities()
         printf("%d. %s\n", i, cities[i]);
     }
 }
+
+
+
+void manageCities()
+{
+    int choice, i, index;
+    char name[50];
+
+    printf("\n1. Add City\n2. Rename City\n3. Remove City\nEnter choice: ");
+    scanf("%d", &choice);
+
+    if(choice == 1)
+    {
+        if(cityCount >= 30)
+        {
+            printf("City limit reached!\n");
+            return;
+        }
+        printf("Enter city name: ");
+        scanf("%s", cities[cityCount]);
+        cityCount++;
+        printf("City added successfully.\n");
+    }
+    else if(choice == 2)
+    {
+        showCities();
+        printf("Enter city index to rename: ");
+        scanf("%d", &index);
+        if(index >= 0 && index < cityCount)
+        {
+            printf("Enter new name: ");
+            scanf("%s", name);
+            int j = 0;
+            while(name[j] != '\0')
+            {
+                cities[index][j] = name[j];
+                j++;
+            }
+            cities[index][j] = '\0';
+            printf("City renamed.\n");
+        }
+        else
+        {
+            printf("Invalid index!\n");
+        }
+    }
+    else if(choice == 3)
+    {
+        showCities();
+        printf("Enter city index to remove: ");
+        scanf("%d", &index);
+        if(index >= 0 && index < cityCount)
+        {
+            for(i = index; i < cityCount - 1; i++)
+            {
+                int j = 0;
+                while(cities[i+1][j] != '\0')
+                {
+                    cities[i][j] = cities[i+1][j];
+                    j++;
+                }
+                cities[i][j] = '\0';
+            }
+            cityCount--;
+            printf("City removed.\n");
+        }
+        else
+        {
+            printf("Invalid index!\n");
+        }
+    }
+}
