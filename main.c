06feaac64ef501f3c5623ce3f5cc32fbd068fa45
Soulrender268451 <as20240565@sci.sjp.ac.lk>
2025-10-26 20:23:23 +0530

    Adding vehicle data arrays

--- main.c
+++ main.c
@@ -3,3 +3,10 @@
 char cities[30][50];
 int cityCount = 0;
 int distanceMatrix[30][30];
+
+
+char vehicleNames[3][10] = {"Van", "Truck", "Lorry"};
+int capacity[3] = {1000, 5000, 10000};
+float rate[3] = {30, 40, 80};
+float speed[3] = {60, 50, 45};
+float efficiency[3] = {12, 6, 4};
